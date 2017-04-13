d8a5740140482b5fce80cac3ceacb3c542789889
chuanwu.sun <chuanwu.sun@ele.me>
2017-04-13 11:38:11 +0800

    refactor: update pypi package

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 entry_points = [
-    "t=PyTodos.todo:cli"
+    "add=pytodos.todo:add",
+    "list=pytodos.todo:list",
+    "got=pytodos.todo:kill"
 ]
 
 setup(
     name="pytodos",
-    version='1.0.1',
-    description="",
+    version='1.0.8',
+    description="a command line lightweight todos tool.",
     long_description="",
     author="chuanwu",
     author_email="chuanwusun@gmail.com",
-    packages=['pytodos'],
+    packages=["pytodos"],
     url="https://github.com/chuanwu/PyToDos.py",
     entry_points={"console_scripts": entry_points},
-    install_requires=[],
-
+    install_requires=[
+        "click==6.7"
+    ],
 )
